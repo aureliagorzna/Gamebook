35d7aaac77e7bb734698d37f4905e74b625feb40
olek03 <olekg48@gmail.com>
2023-05-02 17:53:06 +0200

    fixed usability issues, mario being half playable now

---
+++ models/User.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilepic: {
+        type: mongoose.Types.ObjectId,
+        default: null,
+    },
+    token: {
+        type: Boolean,
+        default: false
+    },
+    JoinedAt: {
+        type: String,
+        immutable: true
+    },
+    description: {
+        type: String,
+        default: "This user has not set the description yet"
+    },
+    points: Number,
+    comments: [{
+        _id: mongoose.Types.ObjectId,
+        user: mongoose.Types.ObjectId,
+        userprof: mongoose.Types.ObjectId,
+        nickname: String,
+        text: String,
+        points: Number,
+        createdAt: {
+            type: String,
+            default: Date.now(),
+            immutable: true
+        },
+        likes: [{
+            type: mongoose.Types.ObjectId,
+        }],
+        photoid: mongoose.Types.ObjectId
+    }],
+    likes: [{
+        type: mongoose.Types.ObjectId,
+    }],
+    posts: [{
+        nickname: String,
+        likes: [{
+            type: mongoose.Types.ObjectId,
+        }],
+        text: String,
+        _id: mongoose.Types.ObjectId,
+        userprof: mongoose.Types.ObjectId,
+        createdAt: Number,
+        points: Number,
+        photoid: mongoose.Types.ObjectId,
+        comments: [{
+            _id: {
+                type: mongoose.Types.ObjectId,
+            },
+            user: mongoose.Types.ObjectId,
+            userprof: mongoose.Types.ObjectId,
+            nickname: String,
+            text: String,
+            points: Number,
+            createdAt: Number,
+            likes: [{
+                type: mongoose.Types.ObjectId,
+            }],
+            photoid: mongoose.Types.ObjectId
+        }]
+    }],
+    saved: [{
+        type: mongoose.Types.ObjectId
+    }]
+})
+
+module.exports = mongoose.model("users", userSchema)
---
+++ routers/userRouters/createRouter.js
@@ -0,0 +1,67 @@
+const express = require("express")
+const router = express.Router()
+const bcrypt = require("bcrypt")
+const User = require("../../models/User")
+
+let taken = 0
+
+router.post("/userCreated", async (req, res) => {
+    try {
+        let today = new Date()
+        let dd = today.getDate()
+        let mm = today.getMonth() + 1
+        let yyyy = today.getFullYear()
+        let hh = today.getHours()
+        let mi = today.getMinutes()
+
+        if (dd < 10) {
+            dd = `0${dd}`
+        }
+        if (mm < 10) {
+            mm = `0${mm}`
+        }
+        if (hh < 10) {
+            hh = `0${hh}`
+        }
+        if (mi < 10) {
+            mi = `0${mi}`
+        }
+
+        today = `${dd}-${mm}-${yyyy} ${hh}:${mi}`
+
+        const usernames = await User.find({}, { name: 1 })
+        let stop = 0
+        usernames.forEach(user => {
+            if (user.name === req.body.nickname) stop = 1
+        })
+
+        if (stop !== 1) {
+
+            const newuser = new User({
+                name: req.body.nickname,
+                password: await bcrypt.hash(req.body.password, 10),
+                JoinedAt: today,
+                points: 0
+            })
+
+            await newuser.save()
+            res.redirect("/users/login")
+        } else {
+            res.redirect("/users/new")
+        }
+
+    } catch (e) {
+        console.error(e.message)
+    }
+})
+
+router.get("/", (req, res) => {
+    if (taken === 1) {
+        taken = 0
+        res.render("userNew", { text: "This name is taken" })
+    } else {
+        res.render("userNew")
+    }
+})
+
+module.exports = router
